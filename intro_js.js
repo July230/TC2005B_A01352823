acc6571b70211b2290bafe87a03cf568703cbc21
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-02-15 10:04:44 -0600

    Funciones JS

--- intro_js.js
+++ intro_js.js
@@ -3,4 +3,9 @@ Ian Julián Estrada Castro
 Archivo de JavaScript
 */
 
-console.log("hola") // Se muestra en consola
+console.log("hola"); // Se muestra en consola
+console.info("Esto es información");
+console.warn("Esto es una advertencia");
+console.error("Esto es un error");
+console.assert(1==1); // Assertion hace evaluaciones
+console.assert(1==2);
