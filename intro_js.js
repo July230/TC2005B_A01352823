c28daaed7a31cb536438a85de6cfcc92a33cc8a3
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-02-15 10:11:07 -0600

    Variables y constantes

--- intro_js.js
+++ intro_js.js
@@ -3,9 +3,20 @@ Ian Julián Estrada Castro
 Archivo de JavaScript
 */
 
-console.log("hola"); // Se muestra en consola
+console.log("hola"); // se muestra en consola
 console.info("Esto es información");
 console.warn("Esto es una advertencia");
 console.error("Esto es un error");
-console.assert(1==1); // Assertion hace evaluaciones
+console.assert(1==1); // assertion hace evaluaciones
 console.assert(1==2);
+
+// variables constantes
+
+// forma antigua de declarar variables
+var juego = "Starbound"; // var tiene acceso a toda la funcion
+
+// constantes
+const precio = 50; 
+
+// forma moderna y mas segura de declarar variables
+const cantidad = 1 // solo acceso a las llaves
