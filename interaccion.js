d291d0cd66473156120be8cb6a6b286d526b9504
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-02-17 18:00:46 -0600

    Funcion promedios

--- interaccion.js
+++ interaccion.js
@@ -55,8 +55,76 @@ function VerificarSuma(){
         }
     }
     // Mostrar tiempo transcurrido en la consola del navegador
-    console.log("Tiempo transcurrido: " + tiempoTranscurrido + " segundos.");
+    console.log("Tiempo transcurrido al sumar es de: " + tiempoTotal + " segundos.");
 }
 
 VerificarSuma();
 
+// Funcion contador
+function contador(numeros) {
+    console.log("Ejercicio 3: El arreglo se llama numeros, adicionalmente hay uno llamado arreglo")
+    // Inicializar contadores
+    let negativos = 0;
+    let ceros = 0;
+    let positivos = 0;
+
+    // Recorrer el arreglo de numeros
+    for(let i = 0; i < numeros.length; i++){
+        if(numeros[i] < 0){
+            negativos++;
+        } else if(numeros[i] === 0){
+            ceros++;
+        } else {
+            positivos++;
+        }
+    }
+
+    // Retornar los resultados como un objeto
+    return{
+        negativos: negativos,
+        ceros: ceros,
+        positivos: positivos
+    }
+    console.log("Cantidad de números negativos: " + negativos);
+    console.log("Cantidad de ceros: " + ceros);
+    console.log("Cantidad de números positivos: " + positivos);
+}
+
+let numeros = [-10, -6, -8, 0, 3, 2, 5, 0, 10];
+let arreglo = [-10, -6, -8, "a", 0, 3, 2, 5, 0, 10, "b", 0, 8, 4];
+
+contador(numeros);
+
+function promedios(matriz){
+    let promedios = [];
+
+    // Recorred cada fila de la matriz
+    for(let i = 0; i < matriz.length; i++){
+        let fila = matriz[i];
+        let suma = 0;
+
+        // Calcular la suma de los elementos en la fila
+        for(let j = 0; j < fila.length; j++){
+            suma += fila[j];
+        }
+
+        // Calcular el promedio de la fila
+        let promedio = suma / (fila.length);
+
+        // Agregar el promedio al arreglo de promedios
+        promedios.push(promedio);
+    }
+    return promedios;
+    
+}
+
+let matriz = [
+    [1, 2, 3],
+    [5, 6, 7],
+    [8, 9, 10],
+    [11, 12, 13],
+    [14, 15, 16]
+];
+
+promedios(matriz);
+console.log("La matriz se llama matriz. Los promedios en cada fila son: " + promedios(matriz));
