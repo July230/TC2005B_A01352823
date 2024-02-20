6c09edbf151005edf8b920743fc605d7bffd3333
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-02-19 19:43:30 -0600

    Funcion password con materialize

---
+++ JS/passwords.js
@@ -0,0 +1,32 @@
+/*
+Ian Julián Estrada Castro
+Archivo JavaScript para erificar contraseñas
+*/
+
+const boton = document.getElementById("validarContrasenia"); 
+
+const password = () => {
+    console.log("click");
+    const contrasenia = document.getElementById("password").value;
+    const confirmarContrasenia = document.getElementById("confirmPassword").value;
+    const MensajeError = document.getElementById("MensajeError");
+
+    // Verificar si las contraseñas coinciden
+    if(contrasenia !== confirmarContrasenia){
+        MensajeError.textContent = "Las contraseñas no coinciden";
+    } else {
+        // Utilizar una expresión regular para comprobar que cuenta con caracteres especiales
+        const patron = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
+        // La contraseña tiene al menos 8 caracteres, al menos una mayuscula, al menos una minuscula, al menos un digito  y al menos un caracter especial
+        if(!patron.test(contrasenia)){
+            MensajeError.textContent = "La contraseña tiene al menos 8 caracteres, al menos una mayuscula, al menos una minuscula, al menos un digito  y al menos un caracter especial";   
+        } else {
+            MensajeError.textContent = "";
+            alert("Contraseña validada correctamente")
+            MensajeError.getElementById("MensajeError").style.visibility = "hidden";
+
+        }
+    }
+}
+
+boton.onclick = password;
