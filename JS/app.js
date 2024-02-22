d718163464842690045766e6265b641132930904
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-02-22 10:17:45 -0600

    Jojo te hackie

--- JS/app.js
+++ JS/app.js
@@ -9,7 +9,9 @@ const filesystem = require('fs');
 // en el archivo indicado en el primer parámetro
 // Con comillas simples es literal
 // Comillas dobles pueden permitir algunos procesos dentro de ese archivos
-filesystem.writeFileSync("hola.txt", "Hola desde node"); // Esto escribirá un "Hola desde node" en un hola.txt en la misma ubicación donde esté, o en su defecto, donde se indique
+filesystem.writeFileSync("hola.txt", "Hola desde node"); // Esto escribirá un "Hola desde node" en un hola.txt en la misma ubicación donde esté, o en su defecto, donde se indique. Se irá reescribiendo en veces posteriores
+
+
 
 
 const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000] // Un arreglo
@@ -20,3 +22,7 @@ for(let item of arreglo){
         console.log(item);
     }, item)
 }
+
+setTimeout(() => {console.log("Jojo te hackié")}, 7000); // Escribirá el mensaje a los 7000 milisegundos
+
+console.log("Esto se imprime antes de los números");
