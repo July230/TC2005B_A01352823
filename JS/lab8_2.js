3cda74bf9601c854beb3f3c1447c2298ebaac5ac
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-02-24 14:18:41 -0600

    Nuevo archivo y modulo http

---
+++ JS/lab8_2.js
@@ -0,0 +1,8 @@
+/*
+Ian Julián Estrada Castro
+María Guadalupe Soria Velázquez
+Laboratorio 8, crear una aplicacion que al hacer una petición, devuelva una de las páginas de los laboratorios
+*/
+
+// Modulo http
+const http = require('http');
