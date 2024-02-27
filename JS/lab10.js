b99bead68cdbd16eff783126ce721cc7efe71363
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-02-27 09:12:56 -0600

    Agregar nueva ruta

--- JS/lab10.js
+++ JS/lab10.js
@@ -29,7 +29,7 @@ const header = `
             <nav class="navbar" role="navigation" aria-label="main navigation">
                 <div class="navbar-brand">
                 <a class="navbar-item" href="/">
-                    <img src="https://static.wikia.nocookie.net/siivagunner/images/a/ac/Starbound.jpg/revision/latest?cb=20190917080905" width="112" height="28">
+                    <img src="https://i1.sndcdn.com/artworks-000109033152-zvlom3-t500x500.jpg" width="112" height="28">
                 </a>
             
                 <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
@@ -47,6 +47,10 @@ const header = `
                 
                     <a class="navbar-item" href="/planetas">
                         Planetas
+                    </a>
+
+                    <a class="navbar-item" href="/enviar">
+                        Enviar planeta
                     </a>
                 
                     <div class="navbar-item has-dropdown is-hoverable">
@@ -129,54 +133,72 @@ const server = http.createServer((request, response) => {
         response.setHeader('Content-Type', 'text/html');
         response.write(header);
         response.write(`
+          <h1 class="title">Hola Mundo Starbound</h1>
+          <p class="subtitle">Mi primer sitio con <strong>Bulma</strong>!</p>
+          <div class="block">
+            <figure class="image is-256x256">
+              <img id="imagen_construir" src="https://i.redd.it/6c1r6kb495mx.jpg" >
+            </figure>
+          </div>
+          <div>
+            <button id="boton_construir" class="button is-primary">Construir casa</button> <!--Un boton para cambiar imagen-->
+          </div>
+          <br>
+        </div>`)
+        response.write(footer);
+        response.end();
+      } else if (request.url == "/planetas" && request.method == "GET") {
+        response.setHeader('Content-Type', 'text/html');
+        response.write(header);
+        response.write(`
             <h1 class="title">Hola Mundo Starbound</h1>
             <div class="columns">
-      `);
+        `);
 
-      let tarjetas_planetas = '';
-      for(let planeta of planetas){
-        tarjetas_planetas += `
-            <div class="column">
-                <div class="card">
-                    <div class="card-image">
-                    <figure class="image is-4by3">
-                        <img src="${planeta.imagen}" alt="Placeholder image">
-                    </figure>
-                    </div>
-                    <div class="card-content">
-                        <div class="media">
-                            <div class="media-left">
-                            <figure class="image is-48x48">
-                                <img src="${planeta.imagen}" alt="Placeholder image">
-                            </figure>
-                            </div>
-                            <div class="media-content">
-                            <p class="title is-4">${planeta.nombre}</p>
-                            <p class="subtitle is-6">@johnsmith</p>
-                            </div>
-                        </div>
-                    
-                        <div class="content">
-                            ${planeta.descripcion}
-                            <a>@bulmaio</a>.
-                            <a href="#">#css</a> <a href="#">#responsive</a>
-                            <br>
-                            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-      }
+        let tarjetas_planetas = '';
+        for(let planeta of planetas){
+          tarjetas_planetas += `
+              <div class="column">
+                  <div class="card">
+                      <div class="card-image">
+                      <figure class="image is-4by3">
+                          <img src="${planeta.imagen}" alt="Placeholder image">
+                      </figure>
+                      </div>
+                      <div class="card-content">
+                          <div class="media">
+                              <div class="media-left">
+                              <figure class="image is-48x48">
+                                  <img src="${planeta.imagen}" alt="Placeholder image">
+                              </figure>
+                              </div>
+                              <div class="media-content">
+                              <p class="title is-4">${planeta.nombre}</p>
+                              <p class="subtitle is-6">@johnsmith</p>
+                              </div>
+                          </div>
+                      
+                          <div class="content">
+                              ${planeta.descripcion}
+                              <a>@bulmaio</a>.
+                              <a href="#">#css</a> <a href="#">#responsive</a>
+                              <br>
+                              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          `;
+        }
       response.write(tarjetas_planetas);
       response.write(`</div>`);
       response.write(footer);
       response.end();
-      } else if (request.url == "/planetas" && request.method == "GET") {
+      } else if (request.url == "/enviar" && request.method == "GET"){
         response.write(header);
         response.write(`
             <h1 class="title">Enviar un planeta</h1>
-            <form action="/planetas" method="POST">
+            <form action="/enviar" method="POST">
                 <label class="label" for="nombre">Tipo</label>
                 <input name="nombre" id="nombre" type="text" class="input"><br>
                 <label class="label" for="imagen">Imagen</label>
@@ -191,7 +213,7 @@ const server = http.createServer((request, response) => {
         // id es del lado del usuario
         response.write(footer);
         response.end();
-      } else if (request.url == '/planetas' && request.method == 'POST') {
+      } else if (request.url == '/enviar' && request.method == 'POST') {
         request.on('data', (dato) => { // on escucha los eventos, cada dato que llega, imprime el dato que le llega al servidor 
             console.log(dato);
         });
@@ -218,7 +240,7 @@ const server = http.createServer((request, response) => {
             
             // Redirigir al usuario a la página principal después de procesar el formulario
             response.statusCode = 302; // Código de estado de redirección
-            response.setHeader('Location', '/'); // URL a la que refirigir 
+            response.setHeader('Location', '/planetas'); // URL a la que refirigir 
             response.end();
 
             
