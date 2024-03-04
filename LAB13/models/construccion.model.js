e112c45c453d430909dc99b94bf54467f5ba15f2
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-03-04 10:13:24 -0600

    Agregar los metodos save y fetchAll

--- LAB13/models/construccion.model.js
+++ LAB13/models/construccion.model.js
@@ -10,10 +10,22 @@ const construcciones = [
 
 // Crear una clase
 module.exports =  class Construccion {
-    // Constructor de una clase. Sirve para crear un nuevo objeto, y en el
+    // Constructor de una clase. Sirve para crear un nuevo objeto, y en el se definen las propiedades del modelo
     constructor(mi_nombre, mi_imagen){
         this.nombre = mi_nombre; // this hace referencia a la instancia actual de la clase
         this.imagen = mi_imagen;
     }
+    // Este metodo servira para guardar de manera persistente el nuevo objeto
+    save(){
+        construcciones.push({
+            nombre: this.nombre,
+            imagen: this.imagen,
+        }); // es lo mismo que construcciones.push(this)
+    }
 
+    // Los estaticos permanecen en memoria, se ejecuta sobre la clase, no sobre un objeto de la clase construccion
+    // Este metodo servira para devolver los objetos del almacenamiento persistente
+    static fetchAll(){
+        return construcciones;
+    }
 }
