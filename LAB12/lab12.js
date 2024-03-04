5f4ced62cb51901a20dad3ad0bdb465f5c31ff33
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-03-03 19:31:58 -0600

    Arreglar rutas y mantener funciones post y get

--- LAB12/lab12.js
+++ LAB12/lab12.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+// Se utiliza EJS como motor de templates para generar html dinámico 
+app.set('view engine', 'ejs'); // Se usa el motor de vistas ejs
+app.set('views', 'views'); // Invicar la carpeta donde están los html
 
 const path = require('path') // Path es un modulo de node
 app.use(express.static(path.join(__dirname, 'public'))); // Darles acceso a la carpeta pública
--- LAB12/routes/planetas.routes.js
+++ LAB12/routes/planetas.routes.js
@@ -8,17 +8,23 @@ const router = express.Router();
 const planetas = [{nombre: "Mundo desértico", imagen: "https://starbounder.org/mediawiki/images/1/18/Desert_Planet.png", descripcion: "Los mundos desérticos se caracterizan por estar cubiertos casi en su totalidad por arena, aun así, es posible hallar vida."}];
 
 // Ahora las rutas llevaran a un archivo tipo ejs, uno que combina html con javaScript de forma válida
-router.get('/planetas', (request, response, next) => {
-    response.render('planetas'); // En lugar de poner el html aquí, redireccionar al html con ejs
+router.get('/', (request, response, next) => {
+    console.log('Ruta /')
+    response.render('starbound'); // En lugar de poner el html aquí, redireccionar al html con ejs
+});
+
+router.get('/enviar', (request, response, next) => {
+    response.render('enviar'); // Hacer render de enviar
 });
 
 router.post('/enviar', (request, response, next) => {
-    console.log(request.body);
-    planetas.push(request.body);
-    response.redirect('/planetas');
+    console.log(request.body); // Hará la petición del cuerpo 
+    planetas.push(request.body); // Agregará la respuesta en el arreglo planetas definido aqui mismo
+    response.redirect('planetas');
 });
-router.get('/', (request, response, next) => {
-    console.log('Ruta /');
+
+router.get('/planetas', (request, response, next) => {
+    console.log('Ruta /planetas');
     response.render('planetas', { // Como no conoce el contenido del arreglo, indicarlo exolícitamente
         planetas: planetas,
     });
