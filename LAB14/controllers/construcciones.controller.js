ce0a6facb47c5d511892bea0f17ec38c7da43eed
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-03-05 10:08:37 -0600

    Hacer copia de lab13 para lab14, usar cookies

---
+++ LAB14/controllers/construcciones.controller.js
@@ -0,0 +1,33 @@
+// Un archivo de controlador, la lógica de como va a interactuar
+
+const Construccion = require("../models/construccion.model");
+
+// El orden en controladores ya no importa porque sólo es lógica
+exports.get_construir = (request, response, next) => { // Para ruta get de construir
+    response.render('construir'); // Hace render a la pagina construir
+};
+
+exports.post_construir = (request, response, next) => { // Para la ruta post
+    console.log(request.body); // Imprime la peticion
+    // Antes se hacia push, pero ahora eso esta en modelo
+    // Request.body es una forma de request que guarda la petición que se hizo
+    const construccion = new Construccion(request.body.nombre, request.body.imagen); // Crear una instancia de la clase
+    construccion.save(); // Se guarda en el arreglo 
+    response.setHeader('Set-Cookie', 'ultima_construccion=', request.body.name); // Definir una cookie
+    response.redirect('/');
+};
+
+exports.get_root = (request, response, next) => { // Para la ruta raiz
+    console.log('Ruta /');
+    let ultima_construccion = request.get('Cookie'); // En caso de que no exista la cookie
+    if(ultima_construccion){ // En caso de que no exista la cookie, un if para así evitar que truene
+        ultima_construccion = ultima_construccion.split('=')[1];
+    } else {
+        ultima_construccion = '';
+    }
+    console.log(ultima_construccion);
+    response.render('construcciones', {
+        construcciones: Construccion.fetchAll(), // Ahora en lugar del arreglo, es la instancia de la clase Construccion
+        ultima_construccion: ultima_construccion, 
+    });
+}
---
+++ LAB14/controllers/planetas.controller.js
@@ -0,0 +1,59 @@
+// Un archivo de controlador, la lógica de como va a interactuar
+// Por controlador hay un modelo
+
+const Planeta = require("../models/planeta.model"); // El modelo que saca
+
+// El orden en controladores ya no importa porque sólo es lógica
+exports.get_registrar = (request, response, next) => { // Para ruta get de registrar planeta
+    response.render('enviar'); // Hace render a la pagina construir
+};
+
+exports.post_registrar = (request, response, next) => { // Para la ruta post
+    console.log(request.body); // Imprime la peticion
+    // Antes se hacia push, pero ahora eso esta en modelo
+    const planeta = new Planeta(request.body.nombre, request.body.imagen, request.body.descripcion); // Crear una instancia de la clase
+    planeta.save(); // Se guarda en el arreglo 
+    response.redirect('/planetas');
+};
+
+
+exports.get_planetas = (request, response, next) => { // Para la ruta que tiene los planetas registrados
+    console.log('Ruta /planetas');
+    response.render('planetas', {
+        planetas: Planeta.fetchAll(), // Ahora en lugar del arreglo, es la instancia de la clase Construccion
+    });
+}
+
+exports.get_root = (request, response, next) => { // Para ruta raiz
+    console.log('Ruta /');
+    response.render('starbound'); // Hace render a la pagina raiz
+};
+
+exports.get_humano = (request, response, next) => { // Para ruta get de humano
+    console.log('Ruta /humano');
+    response.render('humano'); // Hace render a la pagina humano
+};
+
+exports.get_hylotl = (request, response, next) => { 
+    response.render('hylotl'); 
+};
+
+exports.get_avian = (request, response, next) => { 
+    response.render('avian'); 
+};
+
+exports.get_floran = (request, response, next) => { 
+    response.render('floran'); 
+};
+
+exports.get_apex = (request, response, next) => { 
+    response.render('apex'); 
+};
+
+exports.get_glitch = (request, response, next) => { 
+    response.render('glitch'); 
+};
+
+exports.get_novakid = (request, response, next) => { 
+    response.render('novakid'); 
+};
