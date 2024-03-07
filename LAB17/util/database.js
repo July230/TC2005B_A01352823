6e31f1bf7d05f0941792d4e637ba94be4780fae5
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-03-07 10:18:59 -0600

    Instalar mysql2 y configurar database.js

--- LAB17/util/database.js
+++ LAB17/util/database.js
@@ -1,11 +1,11 @@
-/* Este archivo es para interactuar con el gestor SQL*/
+/* Este archivo es para interactuar con el gestor SQL, requerimos el paquete mysql2*/
 const mysql = require('mysql2'); // Se requiere el paquete mysql12
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'database_name', // nombre de la base de datos
-    password: 'el_password_de_tu_usuario_de_la_bd'
+    host: 'localhost', // host de la base de la base de datos, en este caso 127.0.0.1 alias localhost
+    user: 'root', // 
+    database: 'minecraft', // nombre de la base de datos
+    password: '' // lo ideal es que tenga contraseña
 });
 
-module.exports = pool.promise();
+module.exports = pool.promise(); 
