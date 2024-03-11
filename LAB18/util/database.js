68eff1239893d9c74008d9378d155ccbbf4de02b
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-03-11 09:58:32 -0600

    Nueva rama, hacer una copia de lab17 para lab18

---
+++ LAB18/util/database.js
@@ -0,0 +1,13 @@
+/* Este archivo es para interactuar con el gestor SQL, requerimos el paquete mysql2*/
+const mysql = require('mysql2'); // Se requiere el paquete mysql12
+
+const pool = mysql.createPool({
+    host: 'localhost', // host de la base de la base de datos, en este caso 127.0.0.1 alias localhost
+    user: 'root', // 
+    database: 'minecraft', // nombre de la base de datos
+    password: 'JulianSQL' // La contraseña, en caso de que haya
+});
+
+module.exports = pool.promise(); 
+// Las promesas son cuando el gestor hizo o no la consulta, permiten manejar fácilmente código que se ejecuta de manera asíncrona
+
