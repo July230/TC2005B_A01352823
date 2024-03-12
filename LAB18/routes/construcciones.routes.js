74eeb93cbb512ceb501ff50bcc320a55ce648377
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-03-12 09:39:08 -0600

    Proteger las rutas de la aplicacion

--- LAB18/routes/construcciones.routes.js
+++ LAB18/routes/construcciones.routes.js
@@ -2,19 +2,22 @@
 
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../util/is-auth'); // Hacemos la peticion a is-auth.js, el middleware para proteger el sitio
 const construccionesController = require('../controllers/construcciones.controller'); // De donde saca los controladores
 
 
 // Aquí el orden importa, va de middleware en middleware, hay que ir de lo especifico a lo general
-router.get('/construir', construccionesController.get_construir); // Nuevo controlador con get construir
 
-router.post('/construir', construccionesController.post_construir); // Nuevo controlador con post construir
+// Ahora que tenemos que agregar la constante isAuth por cada ruta
+router.get('/construir', isAuth, construccionesController.get_construir); // Nuevo controlador con get construir
+
+router.post('/construir', isAuth, construccionesController.post_construir); // Nuevo controlador con post construir
 
 // En ocasiones es necesario recuperar un registro en particular de la base de datos
 // Para indicarle al ruteador de express que un valor en una ruta es una varibale, podemos hacerlo agregando como prefijo el símbolo :
-router.get('/:construccion_id', construccionesController.get_root);
+router.get('/:construccion_id', isAuth, construccionesController.get_root);
 
-router.get('/', construccionesController.get_root); // Nuevo controlador con get raiz
+router.get('/', isAuth, construccionesController.get_root); // Nuevo controlador con get raiz
 
 // Ahora son unicamente las rutas
 
