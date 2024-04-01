9c8ef87115f5a35c8f7fd82a9882315a8543ad9b
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-04-01 09:20:17 -0600

    Directorio lab 21

---
+++ LAB21/lab19.js
@@ -0,0 +1,57 @@
+/* Archivo principal para laboratorio 19 */
+
+// Es necesario haer iniciado el proyecto con npm init
+// Es necesario instalar body parser
+// Es necesario instalar el motor de templates como EJS
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs'); // Motor de templates
+app.set('views', 'views'); // Views en el directorio views
+
+// Para preparar el entorno para trabajar con sesiones, agregamos como middleware el manejo de sesiones:
+const session = require('express-session'); // Con el modulo express-session
+
+app.use(session({
+  secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste', 
+  resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+  saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+}));
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public'))); // directorio estatico public
+
+const bodyParser = require('body-parser'); // Para manipular facilmente los detos de las peticiones
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Un ataque común es el Cross-Site Request Forgery (CSRF), el cual implica aprovecharse de una sesión de otro usuario, comúnmente perpretado desde una página que parece la oficial pero que en realidad no lo es.
+// Para evitar ataques de CSRF, tenemos que asegurar que nuestros usuarios estén trabajando sobre las vistas que nosotros proveemos. Esto lo podemos lograr por medio de un Token CSRF en nuestras formas y con ayuda de la instalación del paquete csurf.
+const csrf = require('csurf');
+const csrfProtection = csrf();
+app.use(csrfProtection); //...Y después del código para inicializar la sesión... 
+
+
+//Middleware
+app.use((request, response, next) => {
+  console.log('Middleware!');
+  next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+//Registrar el middleware con el módulo construcciones
+const rutasUsuarios = require('./routes/usuarios2.routes'); // Nuevo modulo de rutas usuarios
+app.use('/users', rutasUsuarios);
+
+//Registrar el middleware con el módulo planetas
+const rutasPlanetas = require('./routes/planetas.routes');
+
+app.use('/', rutasPlanetas);
+
+app.use((request, response, next) => {
+  response.status(404);
+  response.sendFile(
+    path.join(__dirname, 'views', '404.html')
+  );
+});
+
+app.listen(3000);
---
+++ LAB21/util/can-view.js
@@ -0,0 +1,16 @@
+/* Middlware que se ejecuta cuando el rol es de visualizador */
+
+module.exports = (request, response, next) => {
+    let can_view = false;
+    for (let permiso of request.session.permisos) {
+        if(permiso.funcion == 'ver') {
+            can_view = true;
+        }
+    }
+    if (can_view) {
+        next();
+    } else {
+        return response.redirect('/users/logout');
+    }
+    
+}
