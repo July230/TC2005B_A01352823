d68cd934625c9d1ce944cecce70bdffe96d9b09c
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-04-02 09:23:05 -0600

    Nuevo directorio para lab24

---
+++ LAB24/models/construccion.model.js
@@ -0,0 +1,38 @@
+/* Archivo de un modelo, el de contruccion */
+
+const db = require('../util/database'); // Ryta a la base de datos
+
+// Crear una clase
+module.exports =  class Construccion {
+    // Constructor de una clase. Sirve para crear un nuevo objeto, y en el se definen las propiedades del modelo
+    constructor(mi_nombre, mi_imagen){
+        this.nombre = mi_nombre; // this hace referencia a la instancia actual de la clase
+        this.imagen = mi_imagen;
+    }
+    // Este metodo servira para guardar de manera persistente el nuevo objeto
+    save(){
+        return db.execute(
+            'INSERT INTO Construccion (nombre, imagen, username) VALUES (?, ?, "rommel49")', // Se dejan los espacios, el uno es para que le ponga al usuario 1
+            [this.nombre, this.imagen] // Para evitar sql inyection se pone el signo de interrogacion en la consulta
+        );
+    }
+
+    // Los métodos estáticos pertenecen a una clase en lugar de una instancia individual de la clase
+    // Este metodo servira para devolver los objetos del almacenamiento persistente
+    // Ahora tiene para extraer de una tabla
+    static fetchAll(){
+        return db.execute('SELECT * FROM Construccion')
+    }
+
+    static fetchOne(id){
+        return db.execute('SELECT * FROM Construccion WHERE id=?', [id]);
+    }
+
+    static fetch(id){
+        if(id){
+            return this.fetchOne(id);
+        } else {
+            return this.fetchAll();
+        }
+    }
+}
