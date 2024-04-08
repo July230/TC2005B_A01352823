4d7c6a1eb45fc97a1879a177af84d3007ea23343
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-04-08 10:29:32 -0600

    Laboratorio 26

---
+++ LAB26/controllers/construcciones.controller.js
@@ -0,0 +1,85 @@
+// Un archivo de controlador, la lógica de como va a interactuar
+
+const { request, response } = require("express");
+const Construccion = require("../models/construccion.model");
+
+// El orden en controladores ya no importa porque sólo es lógica
+exports.get_construir = (request, response, next) => { // Para ruta get de construir
+    response.render('construir', {
+        username: request.session.username || '',
+        csrfToken: request.csrfToken(),
+        permisos: request.session.permisos || [], // Por defecto, un arreglo vacio
+    }); // Hace render a la pagina construir
+};
+
+exports.post_construir = (request, response, next) => { // Para la ruta post
+    console.log(request.body); // Imprime la peticion
+    console.log(request.file); // Información sobre el archivo
+    // Antes se hacia push, pero ahora eso esta en modelo
+    // Request.body es una forma de request que guarda la petición que se hizo
+    const construccion = new Construccion(request.body.nombre, request.file.filename); // Crear una instancia de la clase
+    construccion.save()
+        .then(([rows, fieldData]) => {
+            // Ahora la cookie va adentro del then
+            // Para que la cookie no pueda ser leída por el código js del navegador, se le puede agregar la propiedad HttpOnly
+            response.setHeader('Set-Cookie', 'ultima_construccion=', request.body.name + '; HttpOnly'); 
+            response.redirect('/construcciones');
+
+        }).catch(error => {console.log(error)});
+    }
+
+exports.get_root = (request, response, next) => { // Para la ruta raiz
+    console.log('Ruta /');
+    let ultima_construccion = request.get('Cookie'); // En caso de que no exista la cookie
+    // Para acceder a un valor de una cookie en particular, puedes hacer manipulando el string con split
+    
+    if(ultima_construccion){ // En caso de que no exista la cookie, un if para así evitar que truene
+        ultima_construccion = ultima_construccion.split('=')[1];
+    } else {
+        ultima_construccion = '';
+    }
+    console.log(ultima_construccion);
+    Construccion.fetch(request.params.construccion_id).then(([rows, fieldData]) => { // Se cambia fetchAll por fetch con los parametros definidos en el modelo
+        console.log(rows); // devuelve las filas con su respectivo contenido
+        response.render('construcciones', { // hace render a los registros de construccion
+            construcciones: rows, // Las filas de la tabla construccion
+            ultima_construccion: ultima_construccion, // La cookie ultima construccion
+            username: request.session.username || '', // En caso de que no exista, string vacío
+            permisos: request.session.permisos || [], // Por defecto, un arreglo vacio
+            csrfToken: request.csrfToken(), // Al implementar AJAX y tener una petición tipo post, protegemos con el csrf
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+exports.getConstrucciones = (request, response, next) => {
+    Construccion.fetchAll() // El modelo
+        .then(([rows, fieldData]) => { // then es si se cumple la promesa
+            response.render('vista', {
+                Construccion: rows 
+            })
+        })
+        .catch(err => console.log(err)); // catch es en caso de que no, por lo que el error se muestra en consola
+}
+
+exports.get_buscar = (request, response, next) => {
+    Construccion.search(request.params.valor_busqueda || '')
+        .then(([construcciones, fieldData]) => {
+            // Para enviar las respuestas en formato JSON, en nuestro controlador tenemos que cambiar la respuesta por:
+            return response.status(200).json({construcciones: construcciones});
+        })
+        .catch((error) => {console.log(error)});
+};
+
+exports.post_delete = (request, response, next) => {
+    Construccion.delete(request.body.id)
+        .then(() => {
+            return Construccion.fetch();
+        })
+        .then(([construcciones, fieldData]) => {
+            return response.status(200).json({construcciones: construcciones})
+        })
+        .catch((error) => {console.log(error)});
+};
---
+++ LAB26/models/usuario2.model.js
@@ -0,0 +1,48 @@
+/* Archivo de un modelo, el de Usuario */
+
+const db = require('../util/database_starbound'); // Ryta a la base de datos
+const bcrypt = require('bcryptjs'); // Modulo para cifrar contraseña
+
+
+// Crear una clase
+module.exports =  class Usuario {
+    // Constructor de una clase. Sirve para crear un nuevo objeto, y en el se definen las propiedades del modelo
+    constructor(mi_username, mi_password){
+        this.username = mi_username; // this hace referencia a la instancia actual de la clase
+        this.password = mi_password;
+    }
+    // Este metodo servira para guardar de manera persistente el nuevo objeto
+    save(){
+        return bcrypt.hash(this.password, 12).then((password_cifrado) => { // Con el modulo bcrypt, se cifran los passwords
+            return db.execute(
+                'INSERT INTO Usuario (username, password) VALUES (?, ?)', // Se dejan los espacios
+                [this.username, password_cifrado] // Para evitar sql inyection se pone el signo de interrogacion en la consulta
+            );
+        })
+        .catch((error) => {
+            console.log(error);
+            throw Error('Nombre de usuario duplicado: Ya existe un usuario con ese nombre'); // Devolver un error, este throw hara que en el controlador, vaya al catch sin hacer el then
+        })
+    }
+
+    // Los métodos estáticos pertenecen a una clase en lugar de una instancia individual de la clase
+    // Estos metodos serviran para devolver los objetos del almacenamiento persistente
+    // Ahora tiene para extraer de una tabla
+
+    static fetchOne(username, password){
+        return db.execute('SELECT * FROM Usuario WHERE username=?', // El controlador se encarga de la comparacion de contraseñas
+        [username]);
+    }
+
+    static getPermisos(username){
+        return db.execute(
+            // Username tiene ? para evita SQL inyection
+            `SELECT funcion
+            FROM Usuario u, asigna a, rol r, posee p, permiso per
+            WHERE u.username = ? AND u.username = a.username
+            AND a.idrol = r.id AND r.id = p.idrol
+            AND p.idpermiso = per.id
+            `,
+            [username])
+    }
+}
